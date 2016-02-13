1d8e623b2bf6596823bc61bf8883e9d2064ef766
Alejandro Germenos <agermenos@idgcommunications.com>
2016-02-12 16:57:01 -0800

    Interfaces values

---
+++ com/ocp/interfaces/Fruit.java
@@ -0,0 +1,16 @@
+package com.ocp.interfaces;
+
+
+interface Eatable{
+        int types = 10;
+}
+class Food implements Eatable {
+        public static int types = 20;
+}
+public class Fruit extends Food implements Eatable {
+
+    public static void main(String[] args) {
+        types = 30;
+        System.out.println(types);
+    }
+}
